6e90840a6e4f6dbf02b51e6bd64acb5813f0bd99
Mathis Petitjean <veille.petitjean@gmail.com>
2023-05-13 22:03:37 +0200

    First commit - call api try but failed

--- src/assets/API/ApiCalls.js
+++ src/assets/API/ApiCalls.js
@@ -1,7 +1,7 @@
 import userDatas from "../MockedDatas/user.json"
 
-export async function callApi() {
-    fetch(`../MockedDatas/user.json`)
+export async function callApi(id, endpoint) {
+    fetch(`../MockedDatas/user.json` + {id} + {endpoint})
      .then((res) => {
         if (!res) {
             throw new Error (
@@ -11,3 +11,29 @@ export async function callApi() {
         }
      })
 }
+
+
+export function getEndpointByService(service, userId) {
+    switch (service) {
+      case "activities":
+        return `user/${userId}/performance`;
+  
+      case "average-sessions":
+        return `user/${userId}/average-sessions`;
+  
+      case "daily-activity":
+        return `user/${userId}/activity`;
+  
+      case "firstName":
+        return `user/${userId}`;
+  
+      case "key-data":
+        return `user/${userId}`;
+  
+      case "today-score":
+        return `user/${userId}`;
+  
+      default:
+        return null;
+    }
+  }
